a8f25e406e47eed4ded1179fa3e2004908c35c5d
Qingyuan Liu <qingyuan.liu@goldbeyond.com>
2021-01-20 12:42:45 -0500

    change ip

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ const (
 	//PROJECT_ID = "around-xxx"
 	//BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://34.69.112.113:9200"
+	ES_URL = "http://34.66.250.31:9200"
 )
 
 func main() {
